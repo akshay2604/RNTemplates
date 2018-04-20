571f903b7180f39aaba47a3a0b13d87a23eb0fbb
Akshay Mathur <akshaym@sahusoft.com>
2018-04-20 16:15:28 +0530

    added test template

---
+++ auth/login/email-password.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import { View, Text } from 'react-native';
+
+class EmailPasswordLogin extends Component {
+  render() {
+    <View>
+      <Text>This works</Text>
+    </View>
+  }
+}
+export default EmailPasswordLogin;
